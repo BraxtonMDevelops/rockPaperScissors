4d0a0279883c001d02e16d067e1b7a5893621b2f
BraxtonMDevelops <braxton88888@gmail.com>
2020-05-20 15:24:30 -0400

    ran into an edge case so modified logic

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ game();
     function game(){
         let playerScore = 0;
         let computerScore = 0;
-        while((playerScore || computerScore) < 5){
+        while((playerScore && computerScore) < 5){
 
             let playerMove = prompt("Choose Rock, Paper, or Scissors.");
             let computerMove = computerPlay();
@@ -25,13 +25,13 @@ game();
             alert(`the computer has a score of ${computerScore}`);
         }
         let delta = (computerScore - playerScore);
-        if((computerScore == 5) && (delta > 2)){
+        if((computerScore == 5) && (delta >= 2)){
             alert(`The computer rocked you this game! You lost by ${delta} points!`);        
         }
-        else if ((computerScore == 5) && (delta < 2)){
+        else if ((computerScore == 5) && (delta <= 2)){
             alert(`You lost to the computer but at least it was only by ${delta} points.`);
         }
-        else if ((playerScore == 5) && (delta > -2)){
+        else if ((playerScore == 5) && (delta >= -2)){
             alert(`You beat the computer but it was only by ${Math.abs(delta)} points.`);
         }
         else{
